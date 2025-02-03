93f54de48a45aa5f5a14741fb6f28dc0be745973
Serhii-Polishchuk <serhii.polishchuk@skyhive.io>
2025-02-03 22:13:48 +0200

    add solution for task 4

---
+++ task4/main.py
@@ -0,0 +1,25 @@
+from code_example import draw_tree, Node
+
+def build_tree_from_heap(heap):
+    if not heap:
+        return None
+
+    def build_node(index):
+        if index >= len(heap):
+            return None
+        node = Node(heap[index])
+        node.left = build_node(2 * index + 1)
+        node.right = build_node(2 * index + 2)
+        return node
+
+    return build_node(0)
+
+
+# Приклад використання:
+heap = [10, 20, 15, 30, 40, 50, 100]
+
+# Побудова дерева з купи
+heap_tree_root = build_tree_from_heap(heap)
+
+# Візуалізація дерева
+draw_tree(heap_tree_root)
